86b85adf2629c3f6fa7ec8bdb238256f69cfccd6
luismsanchez <luism.sanchez13@gmail.com>
2021-11-18 22:18:37 -0500

    Views and Test develop

--- Ms1_auth/devices/views.py
+++ Ms1_auth/devices/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import generics
+from .models import DeviceState, DeviceHistory
+from .serializers import DeviceStateSerializer, DeviceHistorySerializer
+
+# Device State
+
+class DeviceStateListCreate(generics.ListCreateAPIView):
+    queryset = DeviceState.objects.all()
+    serializer_class = DeviceStateSerializer
+
+class DeviceStateUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = DeviceState.objects.all()
+    serializer_class = DeviceStateSerializer 
+
+# Device History
+
+class DeviceHistoryListCreate(generics.ListCreateAPIView):
+    queryset = DeviceHistory.objects.all()
+    serializer_class = DeviceHistorySerializer
+
+class DeviceHistoryUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = DeviceHistory.objects.all()
+    serializer_class = DeviceHistorySerializer 
